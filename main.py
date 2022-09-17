e1af477ebb9f2776ec642006359f0c731d06a2e6
David Musaev <vadim566@gmail.com>
2022-09-17 12:41:46 +0300

    confilcts solved

--- main.py
+++ main.py
@@ -44,30 +44,3 @@ if __name__ == '__main__':
             print('calc...' + str(counter))
             time.sleep(1)
 
-
-=======
-if __name__ == "__main__":
-    #Define file path
-    webdriver_path = os.path.normpath(os.path.join(os.getcwd(), 'webdriver', webdriver_executable()))
-    image_path = os.path.normpath(os.path.join(os.getcwd(), 'photos'))
-
-    #Add new search key into array ["cat","t-shirt","apple","orange","pear","fish"]
-    search_keys= ['Glinda, the good Sorceress of Oz, sat in the grand court of her','palace, surrounded by her maids of honor--a hundred of the most'\
-                 ,'beautiful girls of the Fairyland of Oz','The palace court was built of\
-rare marbles', 'exquisitely polished' ]
-
-    #Parameters
-    number_of_images = 2
-    headless = False
-    min_resolution=(0,0)
-    max_resolution=(9999,9999)
-
-    #Main program
-    for search_key in search_keys:
-        image_scrapper = GoogleImageScraper(webdriver_path,image_path,search_key,number_of_images,headless,min_resolution,max_resolution)
-        image_urls = image_scrapper.find_image_urls()
-        image_scrapper.save_images(image_urls)
-    
-    #Release resources    
-    del image_scrapper
->>>>>>> 02a487d8cd0fdfcc61ba6062e8613f90bd165efd
